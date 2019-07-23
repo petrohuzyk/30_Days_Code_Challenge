9685b6254cd6a3678c6327db28e5642b9f732502
petrohuzyk <petroguzik@gmail.com>
2019-07-23 20:54:44 +0300

    added Catalogue Practice

--- Day 10. Binary/task10.cpp
+++ Day 10. Binary/task10.cpp
@@ -1,3 +1,10 @@
+/*
+Task
+Given a base-10 integer, n , convert it to binary (base-2). 
+Then find and print the base-10 integer denoting the maximum number of 
+consecutive 1's in n's binary representation.
+ */
+
 #include <bits/stdc++.h>
 
 using namespace std;
